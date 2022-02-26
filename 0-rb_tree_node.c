43057a9944d4add2112109a63349213e5891300c
root <gnnrbachelor@gmail.com>
2022-02-26 22:25:03 +0000

    Add task 1

---
+++ 0-rb_tree_node.c
@@ -0,0 +1,19 @@
+#include "rb_trees.h"
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *n_node;
+
+
+	n_node = malloc(sizeof(rb_tree_t));
+	if (n_node == NULL)
+		return (NULL);
+	n_node->n = value;
+	n_node->color = color;
+	n_node->left = NULL;
+	n_node->right = NULL;
+	n_node->parent = parent;
+
+	return (n_node);
+
+}
