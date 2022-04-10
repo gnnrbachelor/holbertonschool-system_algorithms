c3f71250f12e3548312181eb5ecfdbf53f01d5ad
root <gnnrbachelor@gmail.com>
2022-04-10 15:02:57 +0000

    Add 3

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -2,5 +2,25 @@
 
 void graph_delete(graph_t *graph)
 {
+	vertex_t *temp_vert;
+	edge_t *temp_edge;
 
+	if (!graph)
+		return;
+
+	while (graph->vertices)
+	{
+		while (graph->vertices->edges)
+		{
+			temp_edge = graph->vertices->edges;
+			graph->vertices->edges = graph->vertices->edges->next;
+			free(temp_edge);
+		}
+		temp_vert = graph->vertices;
+		graph->vertices = graph->vertices->next;
+		free(temp_vert->content);
+		free(temp_vert);
+		graph->nb_vertices -= 1;
+	}
+	free(graph);
 }
