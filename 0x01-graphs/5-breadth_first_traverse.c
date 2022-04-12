f2910bb72444e000b54010ff4f12bb28e10ad4f0
root <gnnrbachelor@gmail.com>
2022-04-12 20:22:50 +0000

    Add 5

--- 0x01-graphs/5-breadth_first_traverse.c
+++ 0x01-graphs/5-breadth_first_traverse.c
@@ -3,7 +3,48 @@
 
 size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth))
 {
+	size_t depth, step, next;
+	vertex_t **vertices;
+	char *visited;
+	edge_t *p;
+	int i, j;
 
+	if (!graph || !action || graph->nb_vertices == 0)
+		return (0);
 
+	visited = calloc(graph->nb_vertices, sizeof(char));
+	vertices = calloc(graph->nb_vertices + 1, sizeof(vertex_t *));
 
+	vertices[0] = graph->vertices;
+
+	visited[0] = 1;
+	depth = 0;
+	j = 1;
+	step = 1;
+	next = 0;
+
+	for (i = 0; vertices[i]; i++)
+	{
+		action(vertices[i], depth);
+
+		for (p = vertices[i]->edges; p; p = p->next)
+		{
+			if (visited[p->dest->index] == 0)
+			{
+				vertices[j++] = p->dest;
+				visited[p->dest->index] = 1;
+				next += 1;
+			}
+		}
+
+		if (--step == 0)
+		{
+			depth++;
+			step = next;
+			next = 0;
+		}
+	}
+	free(visited);
+	free(vertices);
+	return (depth - 1);
 }
--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -78,5 +78,5 @@ void graph_delete(graph_t *graph);
 size_t depth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
 vertex_t *get_vertex_by_index(const graph_t *graph, size_t index);
 void dfs_recursive(int index, size_t *visited, size_t c_depth, size_t *depth, const graph_t *graph, void (*action)(const vertex_t *v, size_t c_depth));
-
+size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
 #endif
