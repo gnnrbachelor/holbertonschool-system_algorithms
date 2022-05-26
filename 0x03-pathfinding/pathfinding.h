a3f390469dc428cc7ba78ccfbe55aba00f393306
root <gnnrbachelor@gmail.com>
2022-05-26 20:03:03 +0000

    Add 3

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <limits.h>
 #include <string.h>
+#include <math.h>
 
 
 #define RIGHT (x + 1)
@@ -16,6 +17,18 @@
 #define LEFT (x - 1)
 #define TOP (y - 1)
 #define STRDUP(x) ((str = strdup(x)) ? str : (exit(1), NULL))
+#define H(a) ((int)sqrt(pow((a->x - target->x), 2) + \
+	pow((a->y - target->y), 2)))
+
+#define ALLOCATE_ALL \
+	do { \
+		dists = calloc(graph->nb_vertices, sizeof(*dists)); \
+		fists = calloc(graph->nb_vertices, sizeof(*fists)); \
+		from = calloc(graph->nb_vertices, sizeof(*from)); \
+		verts = calloc(graph->nb_vertices, sizeof(*verts)); \
+	} while (0)
+
+
 
 
 
@@ -39,5 +52,6 @@ int backtrack_rec(char **map, int x, int y, int rows, int cols, point_t const *s
 
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
 queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+queue_t *a_star_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
 
 #endif
