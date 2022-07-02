aff5fb3a015281ce3b2a46006c36213657c230f1
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-02 17:09:59 +0000

    Add 2

---
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -0,0 +1,40 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_insert - Inserts
+ * @parent: Parent
+ * @str: Content
+ * Return: pointer to to new node
+ */
+
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
+{
+
+	nary_tree_t *n_node;
+
+	n_node = malloc(sizeof(nary_tree_t));
+	if (!n_node)
+		return (NULL);
+
+	n_node->content = strdup(str); 
+	if (!n_node->content)
+		return (NULL);
+
+	n_node->parent = parent;
+	n_node->nb_children = 0;
+	n_node->children = NULL;
+
+	if (parent)
+	{
+		++parent->nb_children;
+		n_node->next = parent->children;
+		parent->children = n_node;
+	}
+	else
+		n_node->next = NULL;
+	
+	return (n_node);
+	
+}
+
+
---
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,28 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - Deletes tree
+ * @tree: Tree
+ * Return: Void
+ *
+ */
+
+void nary_tree_delete(nary_tree_t *tree)
+{
+	nary_tree_t *next = NULL; 
+	nary_tree_t *children = NULL;	
+
+	if (!tree)
+		return;
+	children = tree->children;
+	while (children)
+	{
+		nary_tree_delete(children->children);
+		free(children->content);
+		next = children->next;
+		free(children);
+		children = next;
+	}
+	free(tree->content);
+	free(tree);
+}
