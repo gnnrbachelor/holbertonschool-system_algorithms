e05a499e0a45e4fb89605ae53fd3e64956c4214d
gnnrbachelor <gnnrbachelor@gmail.com>
2022-07-11 13:48:58 +0000

    Add 3

---
+++ 0x04-nary_trees/3-nary_tree_diameter.c
@@ -0,0 +1,47 @@
+#include "nary_trees.h"
+
+static int max_d;
+
+
+static int get_diameter(nary_tree_t *root)
+{
+	nary_tree_t *child = NULL;
+	int max1 = 0, max2 = 0;
+	int cur = 0, diameter = 0;
+
+	if (!root)
+		return (EXIT_FAILURE);
+
+	for (child = root->children; child; child = child->next)
+	{
+		diameter = get_diameter(child);
+		if (cur > diameter)
+			cur = cur;
+		else
+			cur = diameter;
+
+		if (diameter > max1)
+		{
+			max2 = max1;
+			max1 = diameter;
+		}
+		else if (diameter > max2)
+			max2 = diameter;
+	
+	}
+	if ((max1 + max2 + 1) > max_d)
+		max_d = max1 + max2 + 1;
+
+	return (cur + 1);
+
+}
+
+
+size_t nary_tree_diameter(nary_tree_t const *root)
+{
+	if (!root)
+		return (EXIT_FAILURE);
+
+	get_diameter((nary_tree_t *)root);
+	return (max_d);
+}
